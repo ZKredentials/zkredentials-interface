6a1c92151a15da2e0fdbf867f7f4b7608b1c7019
Eric Mong <m.changhsi@gmail.com>
2023-04-16 03:11:53 +0900

    Update color scheme

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -1,10 +1,10 @@
 const colors = {
-  logo: `#00ffff`,
-  title: `#27A9AC`, // blue
+  logo: `#27a9ac`,
+  title: `#27a9ac`, // blue
   text: `#342E37`, // Black
   highlightOne: `#F79F79`, // Orange
   highlightTwo: `#F5F5F5`, // White
-  backgroundOne: `#020202`, // White
+  backgroundOne: `#1e3a63`, // White
   backgroundTwo: `#FFFFFF`, // White
   warning: `#EE4B2B`, // Red
 } as const;
